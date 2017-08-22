660d5f1002c34b821463473e0c21a46af49a1c0f
Marton Demeter <marton1029@gmail.com>
2017-08-22 15:37:48 -0700

    added test file

---
+++ test/test.js
@@ -0,0 +1,13 @@
+const colors = require('../colors-md.js');
+const ansi8 = require('../lib/ansi8.js');
+const ansi256 = require('../lib/ansi256.js');
+
+Object.keys(ansi8).forEach((key) => {
+  process.stdout.write(colors.ansi8.rgb(key)+'▇ ');
+});
+console.log('\n');
+
+ansi256.forEach((color) => {
+  process.stdout.write(colors.ansi256.rgb(`${color.r} ${color.g} ${color.b}`)+'▇ ');
+});
+console.log();
